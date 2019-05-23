01e29016259e5b9680095f8aa8ec01ef77249fc4
tiarra11 <tmcin0329@gmail.com>
2019-05-23 15:06:59 +0000

    updated logo

--- src/js/component/nav.js
+++ src/js/component/nav.js
@@ -3,40 +3,23 @@ import React from "react";
 export default class Nav extends React.Component {
 	render() {
 		return (
-			<nav className="navbar navbar-expand-lg navbar-light bg-light">
-				<a className="navbar-brand" href="#">
-					sTillSports
-				</a>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent"
-					aria-expanded="false"
-					aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon" />
-				</button>
-				<div
-					className="collapse navbar-collapse"
-					id="navbarSupportedContent">
-					<form className="form-inline my-2 my-lg-0">
-						<input
-							className="form-control mr-sm-2"
-							type="search"
-							placeholder="Search for anything"
-							aria-label="Search"
-						/>
-					</form>
-					<form className="form-inline my-2 my-lg-0">
-						<input
-							className="form-control mr-sm-2"
-							type="search"
-							placeholder="All Categories"
-							aria-label="Search"
-						/>
-					</form>
-				</div>
+			<nav className="navbar navbar-light bg-light">
+				<h1>
+					<a className="navbar-brand">sTillSport |</a>
+				</h1>
+				<form className="form-inline">
+					<input
+						className="form-control mr-sm-2"
+						type="search"
+						placeholder="Search"
+						aria-label="Search"
+					/>
+					<button
+						className="btn btn-outline-success my-2 my-sm-0"
+						type="submit">
+						Search
+					</button>
+				</form>
 			</nav>
 		);
 	}
