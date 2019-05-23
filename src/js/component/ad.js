8ac69cf71ed90481748f40f43eea915d80bcb573
tiarra11 <tmcin0329@gmail.com>
2019-05-23 14:35:19 +0000

    ad

--- src/js/component/ad.js
+++ src/js/component/ad.js
@@ -1,14 +1,14 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export default class Ad extends React.Component {
 	render() {
 		return (
-			<div>
-				<p>ad</p>
+			<div className="jumbotron p-4 p-md-5 text-white rounded bg-dark">
+				<img
+					src="https://sportsteamhistory.com/wp-content/uploads/2016/12/Miami-Heat-Logo-600x418.jpeg"
+					className="img-fluid"
+					alt="Responsive image"
+				/>
 			</div>
 		);
 	}
